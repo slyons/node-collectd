944c0fb2ed34ed999dbe8049a6eb832022b0e57e
Bruno Palos <bruno.palos@mindera.com>
2016-03-02 15:14:14 +0000

    Add async support for decoding messages

--- src/decoder.js
+++ src/decoder.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var Readable = require('stream').Readable;
+var util = require('util');
+var Q = require('q');
+
 var clone = require('lodash/clone');
 var ctype = require('ctype');
 var definition = require('./definition');
@@ -13,10 +17,15 @@ var converters = require('./converters');
  * @returns {{type: number, length: number}} A decoded header
  */
 function decodeHeader(buffer, offset) {
-    var type = ctype.ruint16(buffer, 'big', offset);
-    var length = ctype.ruint16(buffer, 'big', offset + 2);
+    var deferred = Q.defer();
 
-    return {type: type, length: length};
+    setImmediate(function () {
+        var type = ctype.ruint16(buffer, 'big', offset);
+        var length = ctype.ruint16(buffer, 'big', offset + 2);
+
+        return deferred.resolve({type: type, length: length});
+    });
+    return deferred.promise;
 }
 
 /**
@@ -28,15 +37,20 @@ function decodeHeader(buffer, offset) {
  * @returns {string} The decoded string
  */
 function decodeStringPart(buffer, offset, partLen) {
-    var stringOffset = definition.HEADER_SIZE + offset;
+    var deferred = Q.defer();
 
-    var decoded = '';
-    for (var i = 0; i < (partLen - 5); i++) {
-        decoded = decoded.concat(String.fromCharCode(ctype.rsint8(buffer, 'big', stringOffset)));
-        stringOffset++;
-    }
+    setImmediate(function () {
+        var stringOffset = definition.HEADER_SIZE + offset;
 
-    return decoded;
+        var decoded = '';
+        for (var i = 0; i < (partLen - 5); i++) {
+            decoded = decoded.concat(String.fromCharCode(ctype.rsint8(buffer, 'big', stringOffset)));
+            stringOffset++;
+        }
+
+        deferred.resolve(decoded);
+    });
+    return deferred.promise;
 }
 
 /**
@@ -47,7 +61,12 @@ function decodeStringPart(buffer, offset, partLen) {
  * @returns {number} The decoded number
  */
 function decodeNumericPart(buffer, offset) {
-    return converters.to64(ctype.rsint64(buffer, 'big', definition.HEADER_SIZE + offset));
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        deferred.resolve(converters.to64(ctype.rsint64(buffer, 'big', definition.HEADER_SIZE + offset)));
+    });
+    return deferred.promise;
 }
 
 /**
@@ -58,8 +77,16 @@ function decodeNumericPart(buffer, offset) {
  * @returns {number} The decoded number
  */
 function decodeHighResolutionPart(buffer, offset) {
-    var highResolution =  decodeNumericPart(buffer, offset);
-    return converters.toLowResolution(highResolution);
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        decodeNumericPart(buffer, offset)
+            .then(function (highResolution) {
+                var lowRes = converters.toLowResolution(highResolution);
+                deferred.resolve(lowRes);
+        });
+    });
+    return deferred.promise;
 }
 
 /**
@@ -70,7 +97,13 @@ function decodeHighResolutionPart(buffer, offset) {
  * @returns {number} The number of values in a values part
  */
 function decodeValuesSize(buffer, offset) {
-    return ctype.ruint16(buffer, 'big', offset + definition.HEADER_SIZE);
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        deferred.resolve(ctype.ruint16(buffer, 'big', offset + definition.HEADER_SIZE));
+    });
+
+    return deferred.promise;
 }
 
 /**
@@ -81,7 +114,12 @@ function decodeValuesSize(buffer, offset) {
  * @returns {number} The type code
  */
 function decodeValueType(buffer, offset) {
-    return ctype.ruint8(buffer, 'big', offset);
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        deferred.resolve(ctype.ruint8(buffer, 'big', offset));
+    });
+    return deferred.promise;
 }
 
 /**
@@ -92,8 +130,13 @@ function decodeValueType(buffer, offset) {
  * @returns {{value: number, type: string}} The decoded counter
  */
 function decodeCounter(buffer, offset) {
-    var counter = ctype.ruint64(buffer, 'big', offset);
-    return {value: converters.to64(counter), type: 'counter'};
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        var counter = ctype.ruint64(buffer, 'big', offset);
+        deferred.resolve({value: converters.to64(counter), type: 'counter'});
+    });
+    return deferred.promise;
 }
 
 /**
@@ -104,8 +147,13 @@ function decodeCounter(buffer, offset) {
  * @returns {{value: number, type: string}} The decoded derive
  */
 function decodeDerive(buffer, offset) {
-    var derive = ctype.rsint64(buffer, 'big', offset);
-    return {value: converters.to64(derive), type: 'derive'};
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        var derive = ctype.rsint64(buffer, 'big', offset);
+        deferred.resolve({value: converters.to64(derive), type: 'derive'});
+    });
+    return deferred.promise;
 }
 
 /**
@@ -116,8 +164,13 @@ function decodeDerive(buffer, offset) {
  * @returns {{value: number, type: string}} The gauge counter
  */
 function decodeGauge(buffer, offset) {
-    var gauge = ctype.rdouble(buffer, 'little', offset);
-    return {value: gauge, type: 'gauge'};
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        var gauge = ctype.rdouble(buffer, 'little', offset);
+        deferred.resolve({value: gauge, type: 'gauge'});
+    });
+    return deferred.promise;
 }
 
 /**
@@ -128,8 +181,13 @@ function decodeGauge(buffer, offset) {
  * @returns {{value: number, type: string}} The decoded absolute
  */
 function decodeAbsolute(buffer, offset) {
-    var absolute = ctype.ruint64(buffer, 'big', offset);
-    return {value: converters.to64(absolute), type: 'absolute'};
+    var deferred = Q.defer();
+
+    setImmediate(function () {
+        var absolute = ctype.ruint64(buffer, 'big', offset);
+        deferred.resolve({value: converters.to64(absolute), type: 'absolute'});
+    });
+    return deferred.promise;
 }
 
 /**
@@ -149,6 +207,12 @@ function getValueDecoder(valueType) {
     return decoder[valueType];
 }
 
+function pushDecodedValue(values, decoded) {
+    values.values.push(decoded.value);
+    values.dstypes.push(decoded.type);
+    values.dsnames.push('value');
+}
+
 /**
  * Decodes the values part.
  *
@@ -157,29 +221,42 @@ function getValueDecoder(valueType) {
  * @returns {{dstypes: Array, values: Array}} A decoded part
  */
 function decodeValuesPart(buffer, offset) {
-    var values = {dstypes: [], values: [], dsnames: []};
+    var deferred = Q.defer();
 
-    // Decode values size
-    var numberOfValues = decodeValuesSize(buffer, offset);
+    setImmediate(function () {
+        var values = {dstypes: [], values: [], dsnames: []};
 
-    var typeOffset = offset + definition.HEADER_AND_LENGTH_SIZE;
-    var valuesOffset = offset + definition.HEADER_AND_LENGTH_SIZE + numberOfValues;
+        // Decode values size
+        decodeValuesSize(buffer, offset)
+            .then(function (numberOfValues) {
+                var typeOffset = offset + definition.HEADER_AND_LENGTH_SIZE;
+                var valuesOffset = offset + definition.HEADER_AND_LENGTH_SIZE + numberOfValues;
 
-    // Decode types
-    for (var i = 0; i < numberOfValues; i++) {
-        var valueType = decodeValueType(buffer, typeOffset);
+                function decode(decoder) {
+                    return decoder(buffer, valuesOffset);
+                }
 
-        var decoder = getValueDecoder(valueType);
-        var decoded = decoder(buffer, valuesOffset);
-        values.values.push(decoded.value);
-        values.dstypes.push(decoded.type);
-        values.dsnames.push('value');
+                function pushDecoded(decoded) {
+                    pushDecodedValue(values, decoded);
 
-        typeOffset++;
-        valuesOffset += definition.VALUE_SIZE;
-    }
+                    typeOffset++;
+                    valuesOffset += definition.VALUE_SIZE;
 
-    return values;
+                    if (values.values.length === numberOfValues) {
+                        deferred.resolve(values);
+                    }
+                }
+
+                // Decode types
+                for (var i = 0; i < numberOfValues; i++) {
+                    decodeValueType(buffer, typeOffset)
+                        .then(getValueDecoder)
+                        .then(decode)
+                        .then(pushDecoded);
+                }
+            });
+    });
+    return deferred.promise;
 }
 
 function addToMetric(metric, typeName, type) {
@@ -227,55 +304,92 @@ function getPartDecoder(partType) {
  * @param offset The offset to start reading
  */
 function decodePart(metrics, metric, header, buffer, offset) {
-    var decoder = getPartDecoder(header.type);
+    var deferred = Q.defer();
 
-    if (decoder === undefined) {
-        throw (new Error('No handler for type ' + header.type));
-    }
+    setImmediate(function() {
+        var decoder = getPartDecoder(header.type);
 
-    var decoded = decoder(buffer, offset, header.length);
-    if (header.type === definition.TYPE_VALUES) {
-        addValuesToMetric(metric, decoded);
-        metrics.push(clone(metric));
-    } else {
-        var typeName = converters.getTypeNameFromCode(header.type);
-
-        if (typeName === undefined) {
-            throw (new Error('No type name found for: ' + header.type));
+        if (decoder === undefined) {
+            deferred.reject(new Error('No handler for type ' + header.type));
         }
 
-        addToMetric(metric, typeName, decoded);
+        decoder(buffer, offset, header.length)
+            .then(function(wDecoded) {
+                if (header.type === definition.TYPE_VALUES) {
+                    addValuesToMetric(metric, wDecoded);
+                    metrics.push(clone(metric));
+                } else {
+                    var typeName = converters.getTypeNameFromCode(header.type);
+
+                    if (typeName === undefined) {
+                        deferred.reject(new Error('No type name found for: ' + header.type));
+                    }
+
+                    addToMetric(metric, typeName, wDecoded);
+                }
+                deferred.resolve({metric: metric, metrics: metrics});
+            });
+    });
+    return deferred.promise;
+}
+
+function Decoder(buffer) {
+    if (!(this instanceof Decoder)) {
+        return new Decoder(buffer);
+    }
+
+    Readable.call(this, {objectMode: true});
+
+    this._metrics = [];
+    this._metric = {};
+    this._offset = 0;
+
+    if(buffer !== undefined) {
+        this._bufferLength = buffer.length;
+        this._buffer = buffer;
+    } else {
+        this.push(null);
     }
 }
+
+util.inherits(Decoder, Readable);
+Decoder.prototype._read = function() {
+    try {
+        if (this._offset < this._bufferLength) {
+            var self = this;
+
+            decodeHeader(this._buffer, this._offset)
+                .then(function(header) {
+
+                    if (header.length === 0) {
+                        self.emit('error', new Error('Unable to decode. Invalid message?'));
+                        return;
+                    }
+
+                    decodePart(self._metrics, self._metric, header, self._buffer, self._offset)
+                        .then(function() {
+                            self._offset += header.length;
+                            self.push(self._metric);
+                        });
+                }, function (err) {
+                    self.emit('error', err);
+                });
+        } else {
+            this.push(this._metrics);
+            this.push(null);
+        }
+    } catch (err) {
+        this.emit('error', err);
+    }
+};
 
 /**
  * Decodes a buffer of collectd metrics encoded in the binary protocol. Please reference to:
  * {@link https://collectd.org/wiki/index.php/Binary_protocol}
  *
  * @param buffer The buffer to use for decoding
- * @returns {Array} An array of decoded collectd metrics.
+ * @returns {Stream} An array of decoded collectd metrics.
  */
-exports.decode = function (buffer) {
-    try {
-        var metrics = [];
-
-        var offset = 0;
-        var bufferLength = buffer.length;
-
-        var metric = {};
-        while (offset < bufferLength) {
-            var header = decodeHeader(buffer, offset);
-            decodePart(metrics, metric, header, buffer, offset);
-
-            if (header.length === 0) {
-                console.error("Unable to decode. Invalid message?");
-                break;
-            }
-
-            offset += header.length;
-        }
-        return metrics;
-    } catch (err) {
-        console.error(err);
-    }
+exports.decode = function(buffer) {
+    return new Decoder(buffer);
 };
